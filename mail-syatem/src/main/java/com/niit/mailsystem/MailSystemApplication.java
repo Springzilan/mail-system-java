2477b4807f59013b0356dffa3dd6988db0f8098e
崔子兰 <springzilan@gmail.com>
2022-04-02 15:05:24 +0800

    init:初始化

---
+++ mail-syatem/src/main/java/com/niit/mailsystem/MailSystemApplication.java
@@ -0,0 +1,13 @@
+package com.niit.mailsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSystemApplication.class, args);
+    }
+
+}
